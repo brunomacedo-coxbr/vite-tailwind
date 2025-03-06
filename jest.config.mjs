90a6ed07e56f7f80fd91376071c83a59d6993ad8
Bruno-Santos <bruno.santos@coxautoinc.com>
2025-03-06 17:27:00 -0300

    chore(setup): initial configuration

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,14 +1,14 @@
 export default {
-  transform: {
+  /* transform: {
     "^.+\\.[t|j]sx?$": "babel-jest",
-    "^.+\\.css$": "jest-transform-stub"
-  },
+  }, */
   testEnvironment: "jsdom",
   transformIgnorePatterns: [
     "/node_modules/(?!your-module-name-to-transform|another-module).+\\.js$"
   ],
   moduleNameMapper: {
-    "\\.css$": "jest-transform-stub"
+    "\\.css$": "jest-transform-stub",
+    "\\.svg$": "jest-transform-stub"
   },
   setupFilesAfterEnv: ["@testing-library/jest-dom"]
 };
